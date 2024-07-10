e6dd02b86710ee33981cbd40a839077d65623c25
Yogesh sahu <73738048+yogass09@users.noreply.github.com>
2024-07-10 16:58:40 +0530

    Update btc_top_charts.R

--- btc_top_charts.R
+++ btc_top_charts.R
@@ -21,84 +21,48 @@ btc_listing_raw <- dbGetQuery(con, paste("SELECT * FROM", "btc_listing_raw"))
 
 ## data frame switch
 
-btc_top_charts <- btc_listing_raw[order(btc_listing_raw$last_updated, decreasing = TRUE), ]
+btc_top_charts <- btc_listing_raw %>%
+  filter(!is.na(circulating_supply) & 
+           !is.na(total_supply) & 
+           !is.na(max_supply) & 
+           !is.na(volume24h)) %>%
+  arrange(desc(last_updated))
+
 
 
                 ## Mayers Multiple ## 
 
-btc_top_charts$ma200.price <- zoo::rollmean(btc_top_charts$price, 200, na.pad = TRUE,align="left")
+btc_top_charts$ma200.price <-round( zoo::rollmean(btc_top_charts$price, 200, na.pad = TRUE,align="left"),2 )
 
-# Calculate Mayer's Multiple
-btc_top_charts$Mayer_Multiple <- btc_top_charts$price / btc_top_charts$ma200.price
-
-
-library(ggplot2)
-
-# Plot Mayer's Multiple
-#ggplot(btc_top_charts, aes(x = last_updated, y = Mayer_Multiple)) +
- # geom_line(color = "green", size = 1) +
- # labs(title = "Mayer's Multiple Over Time",
- #      x = "Date",
- #      y = "Mayer's Multiple") +
- # theme_minimal() +
- # theme(axis.text.x = element_text(angle = 90, hjust = 5))
+# Calculate Mayer's Multiple and round to 2 decimal places
+btc_top_charts$Mayer_Multiple <- round(btc_top_charts$price / btc_top_charts$ma200.price, 2)
 
 
                 ## Puells Multiple ~~
 
 # Assuming your data is in a data frame called 'btc_top_charts'
+
 btc_top_charts <- btc_top_charts %>%
   arrange(last_updated) %>%
-  mutate(daily_issuance = c(0, diff(total_supply))) %>%
-  na.locf()
+  mutate(daily_issuance = na.locf(c(0, diff(total_supply))))
 
-# Calculate 365-day moving average of daily issuance
 btc_top_charts <- btc_top_charts %>%
-  mutate(issuance.ma = zoo::rollmean(daily_issuance, 365, na.pad = TRUE,align = "right"))
+  mutate(issuance.ma = zoo::rollmean(daily_issuance, 365, na.pad = TRUE, align = "right"))
 
-# Calculate Puell Multiple
 btc_top_charts <- btc_top_charts %>%
   mutate(puell_multiple = daily_issuance / issuance.ma)
 
 
-# Plotting Puell Multiple and issuance.ma against price
-library(ggplot2)
-
-# Plot Puell's Multiple with log scale and limited y-axis range
-#ggplot(btc_top_charts, aes(x = last_updated, y = puell_multiple)) +
- # geom_line(color = "blue", size = 1) +
-  #labs(title = "Puell's Multiple Over Time",
-   #    x = "Date",
-    #   y = "Puell's Multiple") +
-  #scale_y_continuous(trans = "log10", limits = c(0.1, 10), breaks = c(0.1, 1, 10)) +
-  #theme_minimal() +
-  #theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
-              ## NVT ## 
+            ## NVT ## 
 
 
 # Calculate the ratio of market cap to daily transaction volume
 btc_top_charts <- btc_top_charts %>% 
   filter(volume24h > 0) %>%
-  mutate(mc_tx_ratio =  market_cap/ volume24h)
-
-library(ggplot2)
-
-# Plot mc_tx_ratio with log scale and limited y-axis range
-#ggplot(btc_top_charts, aes(x = last_updated, y = mc_tx_ratio)) +
- # geom_line(color = "purple", size = 1) +
-  #scale_y_continuous(trans = "log10", limits = c(0.01, 1000), breaks = c(0.01, 0.1, 1, 10, 100, 1000)) +
- # labs(title = "Market Cap to 24h Volume Ratio Over Time",
- #      x = "Date",
- #      y = "Market Cap to Volume Ratio (Log Scale)") +
- # theme_minimal() +
- # theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  mutate(mc_tx_ratio =  round(market_cap/ volume24h),2)
 
 
             ## DrawDown ##  
-
-library(tidyverse)
 
 
 btc_top_charts <- btc_top_charts %>%
@@ -107,23 +71,11 @@ btc_top_charts <- btc_top_charts %>%
 
 # Calculate the maximum drawdown
 max_drawdown <- btc_top_charts %>%
-  summarize(max_drawdown = max(drawdown) * 100) %>%
-  pull()
+  summarize(max_drawdown =max(drawdown) * 100) %>%
+  pull(max_drawdown)
 
 
-#library(ggplot2)
-
-# Plot maximum drawdown
-#ggplot(btc_top_charts, aes(x = last_updated, y = drawdown)) +
- # geom_line(color = "red", size = 1) +
-  #labs(title = "Maximum Drawdown Over Time",
-   #    x = "Date",
-    #   y = "Drawdown (%)") +
- # theme_minimal() +
- # theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-
-          ## Halving Cycles ## 
+      ## Halving Cycles ## 
 
 # Assuming your dataset is named 'btc_top_charts' and the date column is named 'last_updated'
 btc_top_charts <- btc_top_charts %>%
